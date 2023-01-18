1f217b47a436e8eb1d836435c79ed87579007045
QuanLe <zedovblack@gmail.com>
2023-01-18 20:32:40 +0700

    move database username, password to environment variable

--- src/java/db/DBContext.java
+++ src/java/db/DBContext.java
@@ -1,4 +1,4 @@
-package dao;
+package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,16 +11,17 @@ public class DBContext {
     private DBContext() {
     }
 
-    protected static Connection getConnecttion() {
+    public static Connection getConnecttion() {
         Connection connection = DBContext.connection;
         if (connection == null) {
             synchronized (Connection.class) {
                 connection = DBContext.connection;
                 if (connection == null) {
                     try {
-                        String user = "root";
-                        String password = "Leducphi2002";
-                        String url = "jdbc:mysql://localhost:3306/online_learning";
+                        String user = System.getProperty("leon.mysql.username", System.getenv("MYSQL_USERNAME"));
+                        String password = System.getProperty("leon.mysql.password", System.getenv("MYSQL_PASSWORD"));
+                        String database = "online_learning";
+                        String url = "jdbc:mysql://localhost:3306/" + database;
                         Class.forName("com.mysql.cj.jdbc.Driver");
                         DBContext.connection = connection = DriverManager.getConnection(url, user, password);
                     } catch (ClassNotFoundException | SQLException e) {
--- src/java/listener/WebSetvletContextListener.java
+++ src/java/listener/WebSetvletContextListener.java
@@ -4,6 +4,7 @@ import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import java.io.File;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.Enumeration;
@@ -39,6 +40,28 @@ public class WebSetvletContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("Server start");
+        System.setProperty("leon.mysql.username", System.getenv("MYSQL_USERNAME"));
+        System.setProperty("leon.mysql.password", System.getenv("MYSQL_PASSWORD"));
+        String uploadDirectory = System.getenv("UPDIR");
+        if (uploadDirectory == null) {
+            uploadDirectory = System.getProperty("catalina.home") + File.separator + "updir";
+        }
+        String resourceDirectory = uploadDirectory + File.separator + "resource";
+        String thumbnailDirectory = uploadDirectory + File.separator + "thumbnail";
+        String profileDirectory = uploadDirectory + File.separator + "profile";
+        String classDirectory = uploadDirectory + File.separator + "class";
+        String[] dirArray = new String[] {resourceDirectory, thumbnailDirectory, profileDirectory, classDirectory};
+        for (String dir : dirArray) {
+            File dirFile = new File(dir);
+            if (!dirFile.isDirectory()) {
+                dirFile.mkdirs();
+            }
+        }
+        System.setProperty("leon.updir", uploadDirectory);
+        System.setProperty("leon.updir.resource", resourceDirectory);
+        System.setProperty("leon.updir.thumbnail", thumbnailDirectory);
+        System.setProperty("leon.updir.profile", profileDirectory);
+        System.setProperty("leon.updir.class", classDirectory);
     }
     
 }
